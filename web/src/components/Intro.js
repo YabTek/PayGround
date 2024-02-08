48a9d35446717e72e2f2cb63125b624183c553f8
YabTek <yabtekuam@gmail.comremote: Invalid username or password>
2024-02-08 15:54:26 +0300

    change some styles

--- web/src/components/Intro.js
+++ web/src/components/Intro.js
@@ -3,12 +3,14 @@ import img from '../assets/img.png';
 
 const Intro = () => {
   return (
-    <div class="w-full flex-col sm:flex sm:flex-row sm:justify-between bg-[#FAFAEF]">
-      <div class="py-10 px-8 sm:w-1/2">
-        <h2 class="text-[30px] md:text-[46px] lg:text-[80px] md:pt-20 lg:pt-[150px]  font-semibold leading-tight">Finally, a modern way to pay medical bills</h2>
-        <p class="sm:text-sm md:text-xl lg:text-3xl pt-6 mr-10">PayGround simplifies healthcare payments for everyone. Manage, track, and pay—all in one place.</p>
+    <div className="w-full flex-col sm:flex sm:flex-row sm:justify-between bg-[#FAFAEF]">
+      <div className="py-2 px-8 sm:w-1/2">
+        <h2 className="text-[30px] md:text-[40px] lg:text-[60px] xl:text-[80px] sm:pt-10 md:pt-24 lg:pt-36 xl:pt-36 font-semibold leading-tight">Finally, a modern</h2>
+        <h2 className="text-[30px] md:text-[40px] lg:text-[60px] xl:text-[80px] font-semibold leading-tight -mt-2">way to pay</h2>
+        <h2 className="text-[30px] md:text-[40px] lg:text-[60px] xl:text-[80px] font-semibold leading-tight -mt-2">medical bills</h2>
+        <p className="text-md md:text-xl lg:text-2xl pt-6 mr-10">PayGround simplifies healthcare payments for everyone. Manage, track, and pay—all in one place.</p>
       </div>
-      <img class="w-full sm:w-1/2 mb-10 xl:mb-0 sm:mt-8 md:pt-20 px-8" src={img} alt="Introduction" />
+      <img className="w-full sm:w-1/2 mb-10 xl:mb-0 sm:mt-8 md:pt-12 px-8" src={img} alt="Introduction" />
     </div>
   );
 };
